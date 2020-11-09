ff1c60bcdac6906ef9d0cfb24be381adcfd04132
Fatih Boy <fatih@enterprisecoding.com>
2020-11-10 00:22:19 +0300

    Path update

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ mkdir secrets
 echo "admin" > secrets/jenkins-user
 echo "admin" > secrets/jenkins-pass
 
-docker run --name jenkins -d -p 8080:8080 -p 50000:50000  -v ${PWD}/secrets:/var/secrets quay.io/enterprisecoding/jenkins-suggested:latest
+docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v ${PWD}/secrets:/var/secrets quay.io/enterprisecoding/jenkins-suggested:latest
 
 ```
--- security.groovy
+++ security.groovy
@@ -6,8 +6,8 @@ import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 
-def user = new File("/var/secret/jenkins-user").text.trim()
-def pass = new File("/var/secret/jenkins-pass").text.trim()
+def user = new File("/var/secrets/jenkins-user").text.trim()
+def pass = new File("/var/secrets/jenkins-pass").text.trim()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
